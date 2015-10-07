b51f2c96691df3019dfcc5a063ef651055fce818
Dongwook Yoon <dy252@cornell.edu>
2015-10-07 13:46:30 -0400

    course

---
+++ standalone/node_server/routes/course.js
@@ -0,0 +1,29 @@
+/**
+ * Created by yoon on 10/7/15.
+ */
+
+
+var js_error = require("../lib/js_error");
+var R2D = require("../lib/r2d.js");
+var azure = require("../lib/azure");
+var js_utils = require("../lib/js_utils");
+var Promise = require("promise");
+
+var course = {};
+
+course.get = function(req, res){
+
+};
+
+course.submission = {};
+
+course.submission.get = function(req, res){
+
+};
+
+
+course.student = {};
+
+course.student.get = function(req, res){
+
+};

aea47aa2d12b1f2427c5fbf228d9c6f7d638ec52
Dongwook Yoon <dy252@cornell.edu>
2016-01-30 09:50:55 -0500

    metadata done

--- utils/pdf_batch/pdf_batch.py
+++ utils/pdf_batch/pdf_batch.py
@@ -13,15 +13,12 @@ import shutil
 import cPickle
 from difflib import SequenceMatcher
 import subprocess
+import traceback
 
 IMAGE_WIDTH = 1024
 DPI_TO_PX_RATIO = 72
 
-pages = []
-valid_net_ids = []
-
 class PdfsToImages():
-
     @staticmethod
     def run(path):
         print '    Extracting images:',
@@ -31,20 +28,21 @@ class PdfsToImages():
         if not os.path.exists(path_img):
             os.makedirs(path_img)
 
-        n_total_pages = sum(map(lambda x: x.getNumPages(), pdfs))
-        print 'total', n_total_pages, 'pages'
+        print 'total', sum(map(lambda x: x.getNumPages(), pdfs)), 'pages'
 
+        img_paths = []
         n = 1
         for pdf in pdfs:
             for i in xrange(pdf.getNumPages()):
                 img_path = path_img+'/'+str(n)+'.jpg'
+                img_paths.append(img_path)
                 if not os.path.isfile(img_path):
                     print '        '+str(n)+' :', img_path,
                     PdfsToImages.extractImg(pdf = pdf, n = i, out_path = img_path)
                     print '... done!'
                 n += 1
 
-        return n_total_pages
+        return img_paths
 
     @staticmethod
     def getPdfs(path):
@@ -99,107 +97,49 @@ class NetIdValidator():
         if net_id == NetIdValidator.net_ids[l[0]]:
             return net_id
         else:
-            print net_id, 'does not exist in the course roster.'
+            print '   ', net_id, 'does not exist in the course roster. Instead we found similar IDs from our DB.'
             for n, idx in enumerate(l):
-                print n+1, ':', NetIdValidator.net_ids[idx]
-            n = raw_input('Pick one option:')
+                print '       ', n+1, ':', NetIdValidator.net_ids[idx]
+            print '        x : Cancel'
+            n = raw_input('    >>>> Select one option:')
+
+            new_id = ''
             try:
-                return NetIdValidator.net_ids[l[int(n)-1]]
+                new_id = NetIdValidator.net_ids[l[int(n)-1]]
             except Exception:
-                return ''
+                pass
+            print '    Updated to', new_id
+            return new_id
 
-def saveMetadata(pages, filename):
-    l = []
-    for page in pages:
-        d = {};
-        d['to_delete'] = page.to_delete
-        d['net_id'] = page.net_id
-        l.append(d)
-    cPickle.dump(l, open(filename+'.pickle', 'wb'))
 
-def loadMetadata(pages, filename):
-    try:
-        l = cPickle.load(open(filename+'.pickle', 'rb'))
-        for i, d in enumerate(l):
-            pages[i].to_delete = d['to_delete']
-            pages[i].net_id = d['net_id']
-    except Exception:
-        pass
-
-class Page:
-    pdf = None
-    filename = ''
+class PageDisplay:
+    cv_img = None
 
     @staticmethod
-    def setPdf(_pdf, _filename):
-        Page.pdf = _pdf
-        Page.filename = _filename
-        if not os.path.exists(Page.getImageDirPath()):
-            os.makedirs(Page.getImageDirPath())
-
-    @staticmethod
-    def getImageDirPath():
-        return './'+Page.filename.replace('.', '_')
-
-    def __init__(self, n):
-        self.n = n
-        self.to_delete = False
-        self.net_id = ''
-
-        self.cv_img = None
-        pdf_writer = PyPDF2.PdfFileWriter()
-        self.pdf_page = Page.pdf.getPage(n)
-        pdf_writer.addPage(self.pdf_page)
-        self.bytes = io.BytesIO()
-        pdf_writer.write(self.bytes)
-        self.bytes.seek(0)
-        self.rasterizeImage()
-
-    def getImgFilepath(self):
-        return Page.getImageDirPath()+'/'+str(self.n)+'.jpg'
-
-    def rasterizeImage(self):
-        if not os.path.isfile(self.getImgFilepath()):
-            print 'rasterize page:', self.n
-            # rasterize
-            wand_img = Image(file = self.bytes, resolution = int(IMAGE_WIDTH*DPI_TO_PX_RATIO/(self.pdf_page.mediaBox[3])))
-            width, height = wand_img.width, wand_img.height
-            wand_img.depth = 8
-            blob = wand_img.make_blob(format='RGB')
-
-            # convert wand_image to cv_image
-            img = np.zeros((height, width, 3), dtype = np.uint8)
-            for y in xrange(height):
-                for x in xrange(width):
-                    img[y, x, 0] = struct.unpack('B', blob[3*(y*width+x)+2])[0]
-                    img[y, x, 1] = struct.unpack('B', blob[3*(y*width+x)+1])[0]
-                    img[y, x, 2] = struct.unpack('B', blob[3*(y*width+x)+0])[0]
-            cv2.imwrite(self.getImgFilepath(), img)
-
-    def show(self):
-        if self.cv_img == None:
-            self.cv_img = cv2.imread(self.getImgFilepath())
-        cp = self.cv_img.copy()
-        self.putText(
+    def Show( page, n, n_pages_total):
+        #cp = self.cv_img.copy()
+        cp = cv2.imread(page.img_path)
+        PageDisplay.PutText(
             cp,
-            str(self.n+1)+'/'+str(Page.pdf.getNumPages()),
-            (self.cv_img.shape[1]/2, 20)
+            str(n+1)+'/'+str(n_pages_total),
+            (cp.shape[1]/2, 20)
         )
-        if(self.to_delete):
-            self.putText(
+        if(page.to_delete):
+            PageDisplay.PutText(
                 cp,
                 'Deleted',
                 (50, 20)
             )
 
-        self.putText(
+        PageDisplay.PutText(
             cp,
-            self.net_id,
-            (self.cv_img.shape[1]-100, 20)
+            page.net_id,
+            (cp.shape[1]-100, 20)
         )
         cv2.imshow('img', cp)
 
-    def putText(self, img, str, pos):
+    @staticmethod
+    def PutText(img, str, pos):
         boxsize, _ = cv2.getTextSize(str, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, 1)
         cv2.putText(
             img,
@@ -212,11 +152,19 @@ class Page:
             cv2.CV_AA
         )
 
-    def toggleToDelete(self):
+
+class Page:
+    def __init__(self, img_path, metadata):
+        self.to_delete = metadata['to_delete']
+        self.net_id = metadata['net_id']
+        self.img_path = img_path
+
+    def ToggleToDelete(self):
         self.to_delete = not self.to_delete
 
-    def setNetId(self, net_id):
+    def SetNetId(self, net_id):
         self.net_id = net_id
+
 
 def Export(pages, submission_id):
     cur_net_id_path = ''
@@ -252,66 +200,95 @@ def Export(pages, submission_id):
         if not page.to_delete:
             os.remove(cur_net_id_path+'/'+str(page.n)+'.jpg')
 
-
     print '...done!'
 
 
-def init(filename, submission_id):
-    NetIdValidator.init(filename)
-    pdf = PyPDF2.PdfFileReader(file(filename, "rb"))
-    Page.setPdf(pdf, filename)
-    for n in range(pdf.getNumPages()):
-        pages.append(Page(n))
+class PdfBatch():
+    def __init__(self, working_dir):
+        self.cv_img = None
+        self.working_dir = working_dir
+        NetIdValidator.init(working_dir)
+        self.pdf_imgs = PdfsToImages.run(working_dir)
+        self.metadata = self.loadMetadata()
+        self.pages = self.CreatePages(self.pdf_imgs, self.metadata)
+        self.n_total_page = len(self.pages)
 
-    loadMetadata(pages, filename)
+    def run(self):
+        cur_page_n = 0
+        q = False
+        while(not q):
+            PageDisplay.Show(self.pages[cur_page_n], cur_page_n, self.n_total_page)
 
-    cur_page_n = 0
+            key = cv2.waitKey(0)
+            if(key == 27 or key == 113):
+                q = True
+            if(key == 63235): # <Rght-arrow-key>
+                cur_page_n = (1 + cur_page_n)%self.n_total_page
+            if(key == 63234): # <Left-arrow-key>
+                cur_page_n = (self.n_total_page + cur_page_n - 1)%self.n_total_page
+            if key == 63272: # <Delete-key>
+                self.pages[cur_page_n].ToggleToDelete()
+                self.saveMetadata(self.pages)
+            if key == 13: # <Enter-key>
+                net_id = raw_input('    >>>> Enter NetID:')
+                net_id = NetIdValidator.getSimilarNetIds(net_id)
+                self.pages[cur_page_n].SetNetId(net_id)
+                self.saveMetadata(self.pages)
+            if key == 106: # <'J'-key>
+                jump_to_page = raw_input('    >>>>Jump To Page:')
+                cur_page_n = int(jump_to_page)-1
+            if key == 101: # <'E'-key>
+                Export(pages, submission_id)
 
-    q = False
-    while(not q):
-        pages[cur_page_n].show()
-        key = cv2.waitKey(0)
-        if(key == 27 or key == 113):
-            q = True
-        if(key == 63235): # <Rght-arrow-key>
-            cur_page_n = (1 + cur_page_n)%pdf.getNumPages()
-        if(key == 63234): # <Left-arrow-key>
-            cur_page_n = (pdf.getNumPages() + cur_page_n - 1)%pdf.getNumPages()
-        if key == 63272: # <Delete-key>
-            pages[cur_page_n].toggleToDelete()
-            saveMetadata(pages, filename)
-        if key == 13: # <Enter-key>
-            net_id = raw_input('Enter NetID:')
-            net_id = NetIdValidator.getSimilarNetIds(net_id)
-            pages[cur_page_n].setNetId(net_id)
-            saveMetadata(pages, filename)
-        if key == 106: # <'J'-key>
-            jump_to_page = raw_input('Jump To Page:')
-            cur_page_n = int(jump_to_page)-1
-        if key == 113: # <'E'-key>
-            Export(pages, submission_id)
+    def saveMetadata(self, pages):
+        filename = self.working_dir+'/metadata.pickle'
+        l = []
+        for page in pages:
+            d = {}
+            d['to_delete'] = page.to_delete
+            d['net_id'] = page.net_id
+            l.append(d)
+        cPickle.dump(l, open(filename, 'wb'))
 
+    def loadMetadata(self):
+        filename = self.working_dir+'/metadata.pickle'
+        if os.path.isfile(filename):
+            data = cPickle.load(open(filename, 'rb'))
+        else:
+            data = []
+            for i in xrange(len(self.pdf_imgs)):
+                data.append({'net_id':'', 'to_delete': False})
+        print '    Load metadata: total '+str(len(data))+' items'
+        return data
 
-#init('data/math2220_wk13.pdf', 'assignment1')
+    def CreatePages(self, imgs, metadata):
+        if len(imgs) != len(metadata):
+            raise Exception('InvalidMetadata')
+        pages = []
+        for i in xrange(len(imgs)):
+            pages.append(Page(imgs[i], metadata[i]))
+        return pages
+
 
 if __name__ == '__main__':
     try:
         if len(sys.argv) != 2 or not os.path.isdir('data/'+sys.argv[1]):
             raise Exception('InvalidArgs')
-        working_dir = 'data/'+sys.argv[1]
 
-        n_total_pages = PdfsToImages.run(working_dir)
-
-        NetIdValidator.init(working_dir)
+        app = PdfBatch('data/'+sys.argv[1])
+        app.run()
 
     except Exception as e:
         if len(e.args) != 0:
             if e.args[0] == 'InvalidArgs':
                 print '    Urg... This script takes only 1 argument designating the working directory.'
                 print '    Put the scanned pdf(s) into \'data/<folder_name>\', and then run \'python pdf_batch.py <folder_name>.\''
+            elif e.args[0] == 'InvalidMetadata':
+                print '    The number of pages in the metadata mismatches with that of the PDF pages'
+                print '    You may want to delete \'metadata.pickle\' to reset the database and proceed.'
             else:
                 print e
+                traceback.print_exc()
         else:
             print e
-
-
+            traceback.print_exc()

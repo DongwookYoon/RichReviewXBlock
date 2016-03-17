f39d6c6ea7ddb77ab30beb689809d617f84777a3
dongwook <dy252@cornell.edu>
2016-03-17 04:39:35 +0000

    audio bugfix

--- richreview/public/webapps/richreview/r2/audio_utils.js
+++ richreview/public/webapps/richreview/r2/audio_utils.js
@@ -203,7 +203,7 @@
 
         // audio recording initial settings
         pub.RECORDER_SAMPLE_SCALE = 2.5;
-        pub.RECORDER_BUFFER_LEN = 1024;
+        pub.RECORDER_BUFFER_LEN = 4096;
         pub.RECORDER_SAMPLE_RATE = 22050;
         pub.RECORDER_SOURCE_SAMPLE_RATE = 44100;
 

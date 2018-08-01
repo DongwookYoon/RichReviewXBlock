23c7c189c255581a24147bb5aa922b9f7bce2025
cchen795 <cchen795@gmail.com>
2018-07-31 18:36:22 -0700

    clean 5

--- utils/backup_server/env.js
+++ utils/backup_server/env.js
@@ -22,13 +22,13 @@ try {
 } catch(err) {
   console.log("Load ssl from backend server");
   exports.redis_config = JSON.parse(
-    fs.readFileSync(path.join(__dirname, '..', 'ssl/redis_config.json'), 'utf-8')
+    fs.readFileSync(path.join(__dirname, 'ssl/redis_config.json'), 'utf-8')
   );
   exports.azure_config = JSON.parse(
-    fs.readFileSync(path.join(__dirname, '..', 'ssl/azure_config.json'), 'utf-8')
+    fs.readFileSync(path.join(__dirname, 'ssl/azure_config.json'), 'utf-8')
   );
   exports.node_config = JSON.parse(
-    fs.readFileSync(path.join(__dirname, '..', 'ssl/node_config.json'), 'utf-8')
+    fs.readFileSync(path.join(__dirname, 'ssl/node_config.json'), 'utf-8')
   );
   exports.nodemailer_config = JSON.parse(
     fs.readFileSync(

0161a0254fd075306dc87b3a91625cb23b71bcfc
Austin Kobayashi <austinkobayashi@gmail.com>
2019-07-31 14:49:22 -0700

    Working on cwl

--- Frontend/legacy/routes/index.js
+++ Frontend/legacy/routes/index.js
@@ -150,7 +150,6 @@ router.post(
     failureFlash: true
   }),
   function(req, res) {
-    window.open('www.google.ca')
     js_utils.logUserAction(req, 'logged in')
     res.redirect('/education/authentication')
     // res.redirect(req.session.latestUrl || '/')

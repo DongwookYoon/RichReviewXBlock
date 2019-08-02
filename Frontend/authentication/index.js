d8ab03ab759eb9e384c7f39ecbeab76e6f01a611
Austin Kobayashi <austinkobayashi@gmail.com>
2019-08-02 10:21:18 -0700

    Working on cwl

--- Frontend/authentication/index.js
+++ Frontend/authentication/index.js
@@ -1,30 +1,15 @@
-/* eslint-disable no-console */
-const express = require('express')
-const passport = require('passport')
-require('./passport')
-
-// Create express instance
-const app = express()
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-app.get(
-  '/',
-  passport.authenticate('saml', {
-    failureRedirect: '/',
-    failureFlash: true
-  }),
-  function(req, res) {
-    console.log('here')
-    window.open('www.google.ca')
-    const fs = require('fs')
-    fs.writeFileSync('test.txt', 'test')
-    // res.redirect(req.session.latestUrl || '/')
-  }
-)
-
-// app.post(
+// /* eslint-disable no-console */
+// const express = require('express')
+// const passport = require('passport')
+// require('./passport')
+//
+// // Create express instance
+// const app = express()
+//
+// app.use(passport.initialize())
+// app.use(passport.session())
+//
+// app.get(
 //   '/',
 //   passport.authenticate('saml', {
 //     failureRedirect: '/',
@@ -33,12 +18,27 @@ app.get(
 //   function(req, res) {
 //     console.log('here')
 //     window.open('www.google.ca')
+//     const fs = require('fs')
+//     fs.writeFileSync('test.txt', 'test')
 //     // res.redirect(req.session.latestUrl || '/')
 //   }
 // )
-
-// Export the server middleware
-module.exports = {
-  path: '/education/login-ubc',
-  handler: app
-}
+//
+// // app.post(
+// //   '/',
+// //   passport.authenticate('saml', {
+// //     failureRedirect: '/',
+// //     failureFlash: true
+// //   }),
+// //   function(req, res) {
+// //     console.log('here')
+// //     window.open('www.google.ca')
+// //     // res.redirect(req.session.latestUrl || '/')
+// //   }
+// // )
+//
+// // Export the server middleware
+// module.exports = {
+//   path: '/education/login-ubc',
+//   handler: app
+// }
--- Frontend/nuxt.config.js
+++ Frontend/nuxt.config.js
@@ -5,8 +5,8 @@ export default {
   mode: 'universal',
   serverMiddleware: [
     // Will register file from project legacy directory to handle /legacy/* requires
-    { path: '/', handler: '~/legacy/app.js' },
-    { path: '/education/login-ubc', handler: '~/authentication/index.js' }
+    { path: '/', handler: '~/legacy/app.js' }
+    // { path: '/education/login-ubc', handler: '~/authentication/index.js' }
   ],
 
   /*

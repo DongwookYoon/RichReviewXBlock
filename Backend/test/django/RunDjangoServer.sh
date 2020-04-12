0b9c87e1aafd3466945c7082afe1ec5b8e3e9c8d
Oloff <oloff8@hotmail.com>
2020-04-11 23:35:08 -0700

    Configured Travis CI for testing with Mocha

--- Backend/test/django/RunDjangoServer.sh
+++ Backend/test/django/RunDjangoServer.sh
@@ -1,6 +1,6 @@
 
-$name="django-server-rr"
-$port="5000"
+name="django-server-rr"
+port="5000"
 
 # Absolute path to this script
 SCRIPT=$(readlink -f "$0")
@@ -9,6 +9,6 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 
 docker rm -f ${name}
 docker build -t ${name}:1.0 ${SCRIPTPATH}
-docker run -d --name ${name} -p ${port}:${port} -v /tmp:/tmp ${name}:latest
+docker run -d --name ${name} -p ${port}:${port} -v /tmp:/tmp ${name}:1.0
 
 

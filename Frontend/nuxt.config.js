bc1587428c6f489fe64515bb04f7bf8c481f0196
Oloff <oloff8@hotmail.com>
2020-07-01 00:47:51 -0700

    Disable debug

--- Frontend/nuxt.config.js
+++ Frontend/nuxt.config.js
@@ -38,7 +38,7 @@ const config = {
 
   // All environment vars here WILL be exposed on client side via process.env.
   env: {
-    debug_mode: 'true',
+    debug_mode: 'false',
     prod_url: 'https://richreview.net',
     default_canvas: 'canvas.instructure.com',
     canvas_oidc_endpoint: 'https://canvas.ubc.ca/api/lti/authorize_redirect',

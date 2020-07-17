036818d23d9b69c4b798f912fe983188a6f24d84
Oloff <oloff8@hotmail.com>
2020-07-17 03:16:29 -0700

    Store cross domain platform auth URL in sessionStorage

--- Frontend/pages/lti/AssignmentLti.vue
+++ Frontend/pages/lti/AssignmentLti.vue
@@ -389,7 +389,7 @@ export default class AssignmentLti extends Vue {
   public refreshSafely (event: Event) {
     // Break out of event handler context
     window.setTimeout(function () {
-      window.history.back()
+      AssignmentLti.relaunch()
     }, 0)
     event.preventDefault()
     window.removeEventListener('beforeunload', this.refreshSafely) // Prevent infinite loop on unload.
@@ -410,7 +410,7 @@ export default class AssignmentLti extends Vue {
 
     alert('Assignment submitted!')
     window.removeEventListener('beforeunload', this.refreshSafely)
-    window.history.back() // Relaunch after submit.
+    AssignmentLti.relaunch()
   }
 
 
@@ -461,6 +461,10 @@ export default class AssignmentLti extends Vue {
     }
   }
 
+  public static relaunch () {
+    const authUrl : string = window.sessionStorage.getItem('rr_auth_redirect_uri')
+    window.location.assign(authUrl)
+  }
 
   private static getQueryVariable (variable : string, route : string) : string | null {
     const vars : string[] = route.split('&')
